432d30d166a277c1dc5e3588dc1f004d1e9efcd0
Mosseridan <mosseridan@gmail.com>
2018-05-07 09:06:31 +0300

    changed N

--- mat_mul_function_calls3.c
+++ mat_mul_function_calls3.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 1000
+#define N 100000
 
 
 void foo(int i, int j, int** a, int** b, int** c) {
--- mat_mul_loop_unroll2.c
+++ mat_mul_loop_unroll2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 1000
+#define N 100000
 
 int main(int argc, char** argv) {
 
