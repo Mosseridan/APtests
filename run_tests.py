f62608d3a3b41828b0deb840f436b07b5097d4e9
Mosseridan <mosseridan@gmail.com>
2018-08-19 15:44:06 +0300

    added start stride and end to script

--- run_tests.py
+++ run_tests.py
@@ -5,12 +5,6 @@ import distutils
 import time
 from distutils import dir_util
 
-def mkdir_if_none_exits(path):
-    try:
-        os.makedirs(path)
-    except OSError as e:
-        if e.errno != os.errno.EEXIST:
-            raise
 
 def make_batch_file(test_name, test_dir):
     print '\n@@ Making batch script'
@@ -24,27 +18,27 @@ def make_batch_file(test_name, test_dir):
     batch_file.close()
     return batch_file_path
 
-def run_test(test, dest_dir, max_size, times, sbatch):
+
+def run_test(test, dest_dir, start=100, end=300, stride=100, times=1, sbatch=False):
     test_name = os.path.splitext(os.path.basename(test))[0]
-    if(sbatch):
+    if sbatch:
         batch_file = make_batch_file(test_name, dest_dir)     
-        for n in range(1000, max_size+1, 1000):
+        for n in range(start, end+1, stride):
             for i in range(0,times):
                 job_name = test_name+'_'+str(n)+'_'+str(i)
-                print '\n@ running job: '+job_name
+                print('\n@ running job: '+job_name)
                 sub_proc = subprocess.Popen(['sbatch', '-o', job_name, batch_file, test, '-n', str(n), '-j', job_name], cwd=dest_dir)
                 sub_proc.wait()
     else:
-        for n in range(1000, max_size+1, 1000):
+        for n in range(start, end+1, stride):
             for i in range(0,times):
                 job_name = test_name+'_'+str(n)+'_'+str(i)
-                print '\n@ running job: '+job_name
+                print('\n@ running job: '+job_name)
                 sub_proc = subprocess.Popen([test, '-n', str(n), '-j', job_name, '|', 'tee',job_name], cwd=dest_dir)
                 sub_proc.wait()
 
 
-
-def main(dir, max_size, times, sbatch):
+def main(dir, start, end, stride, times, sbatch):
     print '\n@@ Got dir: '+dir   
     print '\n@@ Got times: '+str(times)
 
@@ -52,18 +46,18 @@ def main(dir, max_size, times, sbatch):
     dir_name = os.path.basename(dir)
     timestr = time.strftime("%y%m%d%H%M%S")
     dest_dir = os.path.abspath(os.path.join('results',dir_name+'_results_'+timestr))
-    mkdir_if_none_exits(dest_dir) 
+    os.path.makedirs(dest_dir, exist_ok=True)
     print '\n@@ destination directory: '+dest_dir
 
     for root, dirs, files in os.walk(dir):
         for name in files:
-            if os.path.splitext(name)[1] == '.out':
+            split_ext = os.path.splitext(name)
+            if split_ext[1] == '.out':
                 test = os.path.join(root,name)
-                print "\n@@ runing test: "+test
-                run_test(test, dest_dir, max_size, times, sbatch)
-
-
-
+                print("\n@@ runing test: "+test)
+                test_dest_dir = os.path.join(dest_dir, split_ext[0])
+                os.path.makedirs(test_dest_dir, exist_ok=True)
+                run_test(test, test_dest_dir, start, end, stride, times, sbatch)
 
 
 if __name__ == '__main__':
@@ -71,9 +65,17 @@ if __name__ == '__main__':
     parser.add_argument('-dir',
         dest='dir',
         help='Path to the directory containing the tests.')
-    parser.add_argument('-n',
+    parser.add_argument('-start',
         type=int,
-        dest='max_size',
+        dest='start',
+        help='Maximum input size.')
+    parser.add_argument('-end',
+        type=int,
+        dest='end',
+        help='Maximum input size.')
+    parser.add_argument('-stride',
+        type=int,
+        dest='stride',
         help='Maximum input size.')
     parser.add_argument('-t',
         type=int,
@@ -85,6 +87,5 @@ if __name__ == '__main__':
         help='Run with sbatch.')
     args = parser.parse_args()
 
-    main(args.dir, args.max_size,args.times, args.sbatch)
-
-    print "\n@@ Done"
+    main(args.dir, args.start, args.end, args.staride, args.times, args.sbatch)
+    print("\n@@ Done")
