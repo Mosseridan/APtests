ae687edefba95cf79fccf4091979003a0c2ed759
Mosseridan <mosseridan@gmail.com>
2018-08-21 13:37:12 +0300

    fixed default parameters and added slurm partition parameter

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,7 @@ from distutils import dir_util
 
 
 def make_batch_file(test_name, test_dir):
-    print '\n@@ Making batch script'
+    print('\n@@ Making batch script')
     batch_file_path = os.path.join(test_dir, 'batch_job.sh')
     batch_file = open(batch_file_path,'w')
     batch_file.write(
@@ -19,7 +19,7 @@ def make_batch_file(test_name, test_dir):
     return batch_file_path
 
 
-def run_test(test, dest_dir, start=100, end=300, stride=100, times=1, sbatch=False, slurm_partition='nxtscl'):
+def run_test(test, dest_dir, start=100, end=100, stride=100, times=1, sbatch=False, slurm_partition='nxtscl'):
     test_name = os.path.splitext(os.path.basename(test))[0]
     if sbatch:
         batch_file = make_batch_file(test_name, dest_dir)     
@@ -39,16 +39,16 @@ def run_test(test, dest_dir, start=100, end=300, stride=100, times=1, sbatch=Fal
                 sub_proc.wait()
 
 
-def main(dir=, start=100, end=200, stride=100, times=1, sbatch=False, slurm_partition='nxtscl'):
-    print '\n@@ Got dir: '+dir   
-    print '\n@@ Got times: '+str(times)
+def main(dir, start=100, end=200, stride=100, times=1, sbatch=False, slurm_partition='nxtscl'):
+    print('\n@@ Got dir: '+dir)
+    print('\n@@ Got times: '+str(times))
 
     dir = os.path.abspath(dir)
     dir_name = os.path.basename(dir)
     timestr = time.strftime("%y%m%d%H%M%S")
     dest_dir = os.path.abspath(os.path.join('results',dir_name+'_results_'+timestr))
-    os.path.makedirs(dest_dir, exist_ok=True)
-    print '\n@@ destination directory: '+dest_dir
+    os.makedirs(dest_dir, exist_ok=True)
+    print('\n@@ destination directory: '+dest_dir)
 
     for root, dirs, files in os.walk(dir):
         for name in files:
@@ -57,7 +57,7 @@ def main(dir=, start=100, end=200, stride=100, times=1, sbatch=False, slurm_part
                 test = os.path.join(root,name)
                 print("\n@@ runing test: "+test)
                 test_dest_dir = os.path.join(dest_dir, split_ext[0])
-                os.path.makedirs(test_dest_dir, exist_ok=True)
+                os.makedirs(test_dest_dir, exist_ok=True)
                 run_test(test, test_dest_dir, start, end, stride, times, sbatch, slurm_partition)
 
 
@@ -67,18 +67,30 @@ if __name__ == '__main__':
         dest='dir',
         help='Path to the directory containing the tests.')
     parser.add_argument('-start',
+        nargs='?',
+        const=1,
+        default=1,
         type=int,
         dest='start',
         help='Maximum input size.')
     parser.add_argument('-end',
+        nargs='?',
+        const=1,
+        default=1,
         type=int,
         dest='end',
         help='Maximum input size.')
     parser.add_argument('-stride',
+        nargs='?',
+        const=1,
+        default=1,
         type=int,
         dest='stride',
         help='Maximum input size.')
     parser.add_argument('-t',
+        nargs='?',
+        const=1,
+        default=1,
         type=int,
         dest='times',
         help='Number of times each test will be executed.')
@@ -87,6 +99,10 @@ if __name__ == '__main__':
         dest='sbatch',
         help='Run with sbatch.')
     parser.add_argument('-p',
+        nargs='?',
+        const=1,
+        default='nxtscl',
+        type=str,
         dest='slurm_partition',
         help='Slurm partition to use with sbatch.')
     args = parser.parse_args()
